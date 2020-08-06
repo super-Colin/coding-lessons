1eabe14d0ca223436c7ddd5c952b5f51ba8f0664
superColin <supercolin.dev@gmail.com>
2020-08-06 11:16:00 -0400

    uncommented examples

--- basics2.chalkboard.py
+++ basics2.chalkboard.py
@@ -1,40 +1,37 @@
 
+# print('hello world!!!!!!!!!!')
 
-# # print('hello world!!!!!!!!!!')
+i = 0
 
-# i = 0
+if i < 4:
+    f = 'something'
+    print(i)
+    i = i + 1
+    return f
 
-# if i < 4:
-#     f = 'something'
-#     print(i)
-#     i = i + 1
-#     return f
+## the return part of functions:
+def returner(number):
+    return number + 5
 
-### the return part of functions:
-# def returner(number):
-#     return number + 5
+newVar = returner(7)
 
-# # newVar = returner(7)
-
-# def upperMath(anotherNum):
-#     return anotherNum % 3
+def upperMath(anotherNum):
+    return anotherNum % 3
 
 
-# # print(newVar)
-# # print(returner(12))
-# print(upperMath(returner(9)))
+print(newVar)
+print(returner(12))
+print(upperMath(returner(9)))
 
 ### Arrays:
 
 arrayCist = [ 'first', 34, 345, 234, 546518, 64, 'last']
 
-# print(arrayCist[0])
+print(arrayCist[0])
 
-# arrayCist[0] = 'not first anymore'
+arrayCist[0] = 'not first anymore'
 
-# print(arrayCist[0])
+print(arrayCist[0])
 
 for cist in arrayCist:
     print(cist)
-
-
